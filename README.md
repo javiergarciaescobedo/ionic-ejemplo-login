bab4a543a5749887436c91732cfb42d28a329586
Javier García Escobedo <javiergarbedo@gmail.com>
2020-02-10 14:14:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Ejemplo de Login con Firebase en Ionic
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage {
+
+  userEmail: String = "";
+  userUID: String = "";
+  isLogged: boolean;
+
+  constructor(
+    public loadingCtrl: LoadingController,
+    private authService: AuthService,
+    private router: Router,
+    public afAuth: AngularFireAuth
+  ) { }
+
+  ionViewDidEnter() {
+    this.isLogged = false;
+    this.afAuth.user.subscribe(user => {
+      if(user){
+        this.userEmail = user.email;
+        this.userUID = user.uid;
+        this.isLogged = true;
+      }
+    })
+  }
+
+  login() {
+    this.router.navigate(["/login"]);
+  }
+
+  logout(){
+    this.authService.doLogout()
+    .then(res => {
+      this.userEmail = "";
+      this.userUID = "";
+      this.isLogged = false;
+      console.log(this.userEmail);
+    }, err => console.log(err));
+  }
+
+}
